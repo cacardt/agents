4a6fa1c9cd8ae3a33dad401e12983ce8eabaa7fc
Théo Cacard <Theo.Cacard@etu.univ-savoie.fr>
2023-12-08 23:23:50 +0100

    start of agent strategy

--- src/agent.py
+++ src/agent.py
@@ -1,4 +1,7 @@
 import requests
+URL_API = "http://127.0.0.1:5000"
+number = [1]
+
 
 class Agent:
     def __init__(self, cid, teamid, life, strength, speed, armor):
@@ -8,21 +11,47 @@ class Agent:
         self.strength = strength
         self.speed = speed
         self.armor = armor
-
-    def call_api(url, headers=None, params=None):
-        response = requests.get(url, headers=headers, params=params)
+    
+    def call_api(self, url, method, data=None):
+        if method == 'GET':
+            response = requests.get(url)
+        elif method == 'POST':
+            response = requests.post(url, json=data)
         data = response.json()
+        print("data", data)
         return data
 
     def act(self, observation):
-
+        lowerLife = 20
+        lowerPlayer = ""
+        action = 0
+        playersIn = observation[0]
+        arenaState = observation[1]
+        print("État de l'arène", arenaState)
+        self.call_api(f"{URL_API}/character/join", "POST", {"cid": self.cid, "teamid": self.teamid, "life": self.life, "strength": self.strength, "speed": self.speed, "armor": self.armor})
+        for i in range(len(playersIn)):
+            if(playersIn[i]["cid"] != self.cid):
+                if(playersIn[i]["life"] < lowerLife and self.life >= 5):
+                    lowerLife = playersIn[i]["life"]
+                    lowerPlayer = playersIn[i]["cid"]
+                    action = 0
+                elif(self.life < 5):
+                    lowerPlayer = ""
+                    action = 1
+                else:
+                    lowerPlayer = ""
+                    action = 2
+        self.call_api(f"{URL_API}/character/action", "POST", {"cid": self.cid, "target": lowerPlayer, "action": action})
         # sélectionne et exécute une action en fonction de l'observation, qui est l'état de l'arène.
         # il faut ici faire appel aux routes de l'API !
-        pass
 
     def observe(self):
+        observation = []
+        observation.append(self.call_api(f"{URL_API}/characters", "GET"))
+        observation.append(self.call_api(f"{URL_API}/arenaState", "GET"))
+
         # récupère l'état de l'arène et le retourne
-        return 0
+        return observation
 
     def isAlive(self):
         # retourne vrai si life > 0, false sinon
--- src/main.py
+++ src/main.py
@@ -1,16 +1,30 @@
 from agent import *
+import random
 import time
 
 if __name__ == '__main__':
-    agent1 = Agent("exemple", "exempleEquipe", 10, 7, 7, 6)
-    agent2 = Agent("exemple2", "exempleEquipe", 10, 7, 7, 6)
+    agent1 = Agent("exemple", "exempleEquipe", 10, 2, 2, 6)
+    agent2 = Agent("exemple2", "exempleEquipe", 10, 4, 2, 4)
+    # create 10 agents with random stats each whose sum equals 20
+    life = 0
+    strength = 0
+    speed = 0
+    armor = 0
+    agents = []
+    for i in range(10):
+        while(life + strength + speed + armor != 20):
+            life = random.randint(1, 10)
+            strength = random.randint(1, 10)
+            speed = random.randint(1, 10)
+            armor = random.randint(1, 10)
+        agents.append(Agent(str(i), "exempleEquipe", life, strength, speed, armor))
+    agents.append(agent1)
+    agents.append(agent2)   
 
-    while agent1.isAlive() or agent2.isAlive():
+# for loop for every agent
 
-        if agent1.isAlive():
-            obs = agent1.observe()
-            agent1.act(obs)
-
-        if agent2.isAlive():
-            obs = agent2.observe()
-            agent2.act(obs)
+while agents[0].isAlive() or agents[1].isAlive() or agents[2].isAlive() or agents[3].isAlive() or agents[4].isAlive() or agents[5].isAlive() or agents[6].isAlive() or agents[7].isAlive() or agents[8].isAlive() or agents[9].isAlive() or agents[10].isAlive() or agents[11].isAlive():
+    for i in range(len(agents)):
+        if agents[i].isAlive():
+            obs = agents[i].observe()
+            agents[i].act(obs)
